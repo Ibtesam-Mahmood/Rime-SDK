9eeceb132c301ebca1b86f16f89c6ca96c2004a5
Mkohler4 <Mkohler4@uwo.ca>
2021-03-02 02:43:56 -0500

    updated chatTile and loader

--- lib/components/DefaultWidgets/chatTile.dart
+++ lib/components/DefaultWidgets/chatTile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:rime/components/widgets/overLappingProfilePictures.dart';
-import 'package:rime/components/widgets/wrappedListTile.dart';
+import 'package:rime/components/DefaultWidgets/wrappedListTile.dart';
+import 'package:rime/components/DefaultWidgets/overLappingProfilePictures.dart';
 
 class ChatTile extends StatefulWidget {
 
@@ -34,14 +34,28 @@ class _ChatTileState extends State<ChatTile> {
       child: WrappedListTile(
         contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         //TODO: Add state to title and subtitle
-        title: "Markus",
-        subtitle: Text("Message from Markus"),
-        leading: OverlappingProfilePicture(),
+        title: "Killua",
+        subtitle: Text("Message from Killua"),
+        leading: OverlappingProfilePicture(
+          topImage: 'https://i.pinimg.com/736x/2d/11/a3/2d11a390094c8851ec366c4742d37f1c.jpg',
+          bottomImage: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c73969f3-13d3-40b3-81e6-e847df80e3ca/d85u45x-d3a65754-e859-48f3-a736-07741d2b376a.png/v1/fill/w_1024,h_1211,strp/hunter_x_hunter___gon___updated_head_by_daul_d85u45x-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMjExIiwicGF0aCI6IlwvZlwvYzczOTY5ZjMtMTNkMy00MGIzLTgxZTYtZTg0N2RmODBlM2NhXC9kODV1NDV4LWQzYTY1NzU0LWU4NTktNDhmMy1hNzM2LTA3NzQxZDJiMzc2YS5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.BaaoW_E9bLMs951VPza6kcwJoxbtk1osHLK34xJe-F4',
+          imageSize: 40,
+          height: 50,
+          width: 50,
+        ),
         //TODO: Implement unread and muted chats
-        trailing: Container(
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            shape: BoxShape.circle
+        trailing: SizedBox(
+          height: 12,
+          width: 12,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle
+              ),
+            ),
+            ]
           ),
         ),
         onTap: (){
