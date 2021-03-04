657ed13ead8e7d530158389831c4c5fe42ea15b5
Christopher Guay <35672191+ChristopherGuay@users.noreply.github.com>
2021-03-04 17:53:14 -0500

    Reorganized and added more tests
    Added tests to
    - print all channel groups for a user
    - print all channels for a user
    - print all messages in a specific channel
    - ensure a specific message in a specific channel exists
    - add a message action and receive a successful http response
    
    Co-Authored-By: Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>

--- test/rime_test.dart
+++ test/rime_test.dart
@@ -11,6 +11,7 @@ import 'package:rime/state/RimeRepository.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
 void main() async {
+  // I believe this was example flutter test code
   // test('adds one to input values', () {
   //   final calculator = Calculator();
   //   expect(calculator.addOne(2), 3);
@@ -19,48 +20,114 @@ void main() async {
   //   expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   // });
 
-  group('String test tests', () {
-    test('String.split() splits the string on the delimiter', () {
-      var string = 'foo,bar,baz';
-      expect(string.split(','), equals(['foo', 'bar', 'baz']));
-    });
-
-    test('String.trim() removes surrounding whitespace', () {
-      var string = '  foo ';
-      expect(string.trim(), equals('foo'));
-    });
-  });
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('String test tests', () {
+  group('RimeApi Tests: ', () {
     setUp(() async {
       HttpOverrides.global = null;
-      print('Initializing');
+      print('\n' 'Initializing');
       await DotEnv.load(fileName: '.env');
       await Rime.initialize(DotEnv.env);
       await RimeRepository().initializeRime('testUser1');
       print('Initialized');
     });
 
-    test('test', () async {
-      // await RimeApi.createChannel(['testUser1', 'testUser2']);
+    test('Print all groups for testUser1', () async {
+      String userID = 'testUser1';
+      await printAllGroupsForUser(userID);
+    }, skip: "Don't want this to print everytime");
 
-      // List<String> g = await RimeApi.getChannelGroups('testUser1');
+    test('Print all groups and channels for testUser1', () async {
+      String userID = 'testUser1';
+      await printAllGroupsAndChannelsForUser(userID);
+    }, skip: "Don't want this to print everytime");
 
-      // print('Groups: ' + g.toString());
+    group('Specific message and channel group tests', () {
+      int timeToken = 16148934155829055;
+      String channel = 'rime_testUser1_16148386848078668';
 
-      // ChannelGroupListChannelsResult groups =
-      //     await RimeRepository().client.channelGroups.listChannels(g[0]);
-      // print('Channels: ' + groups.channels.toList().toString());
+      test('Print all messages from the channel: $channel ', () async {
+        await printEveryMessageInAChannel(channel);
+        var actual = true;
+        var expected = true;
+        expect(actual, expected);
+      }, skip: "Don't want it to print every time");
 
-      print('hello');
+      test(
+          'Checks that the message on channel: $channel, at time: $timeToken exists',
+          () async {
+        PaginatedChannelHistory ch =
+            RimeRepository().client.channel(channel).history();
 
-      await RimeRepository().client.addMessageAction("userID", "true", "rime_testUser1_16148386848078668", Timetoken(16148575074308885));
+        await ch.more();
 
-      await Future.delayed(Duration(seconds: 10));
+        List<BaseMessage> messages = ch.messages;
 
-      expect(true, true);
+        BaseMessage message = messages
+            .firstWhere((e) => e.publishedAt.value == 16148934155829055);
+        var actual = message;
+        var expected = isNot(null);
+        expect(actual, expected);
+      });
+    });
+
+    test('Add Message Action', () async {
+      //Send Message
+      var channel = 'rime_testUser1_16148386848078668';
+      var message = 'hello';
+      PublishResult publish =
+          await RimeRepository().client.publish(channel, message);
+
+      //Get the TimeToken for the message that was just sent
+      int time = publish.timetoken;
+
+      //Add Test messageAction to the just sent message
+      AddMessageActionResult res = await RimeRepository()
+          .client
+          .addMessageAction('test', 'true', channel, Timetoken(time));
+
+      //Confirm that it receives a successful response
+      var actual = res.status;
+      var expected = 200;
+      expect(actual, expected);
     });
   });
+}
+
+Future printEveryMessageInAChannel(String channel) async {
+  PaginatedChannelHistory ch =
+      RimeRepository().client.channel(channel).history();
+
+  await ch.more();
+
+  List<BaseMessage> messages = ch.messages;
+
+  BaseMessage message;
+  for (message in messages) {
+    print('Time: ' +
+        message.publishedAt.toString() +
+        '\n\t' +
+        message.content.toString());
+  }
+}
+
+Future printAllGroupsAndChannelsForUser(String userID) async {
+  List<String> g = await RimeApi.getChannelGroups(userID);
+
+  String groupName;
+  for (groupName in g) {
+    await printAllChannelsInAGroup(groupName);
+  }
+}
+
+Future printAllChannelsInAGroup(String groupName) async {
+  ChannelGroupListChannelsResult channelGroupList =
+      await RimeRepository().client.channelGroups.listChannels(groupName);
+  print('Group: ' + channelGroupList.name);
+  print('Channels: ' + channelGroupList.channels.toList().toString());
+}
+
+Future printAllGroupsForUser(String userID) async {
+  List<String> g = await RimeApi.getChannelGroups(userID);
+  print('Groups: ' + g.toString());
 }
