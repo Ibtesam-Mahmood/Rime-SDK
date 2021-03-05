17bc30ef4f73dbcc7b8d11e40b041e74644a5a28
Mkohler4 <Mkohler4@uwo.ca>
2021-03-04 20:14:59 -0500

    Edit Rime API

--- lib/api/rime_api.dart
+++ lib/api/rime_api.dart
@@ -9,12 +9,14 @@ class RimeApi {
   static Future<List<RimeChannel>> getChannels(String loginID,
       {int timeToken}) {}
 
-  static Future<String> createChannel(List<String> users) async {
+  static Future<RimeChannel> createChannel(List<String> users) async {
+    
     //Checks if the user iD's are unique
     if (users.length != users.toSet().length) {
       return Future.error("Duplicate user's within channel creation");
     }
     
+    //Access Pubnub Client
     PubNub client = RimeRepository().client;
 
     //Creates a unique channel ID
@@ -22,34 +24,59 @@ class RimeApi {
     String channelID = 'rime_${RimeRepository().userID}_${time.toString()}';
     List<ChannelMemberMetadataInput> members = [];
 
+    // Group ID for the loggedIn user
+    String userGroupID;
+
+    // Memmebership for the user
+    RimeChannelMemebership userMembership;
+
     // Creates memeberships for a channel
     for (String userID in users) {
-      members.add(ChannelMemberMetadataInput(
-        userID,
-        custom: {
-          //TODO: Notifications Enabled?
-          // 'Notification': true,
-          'readAction': true,
-          'Accepted': Rime.functions.chatAccepted(RimeRepository().userID, users),
-          'Deleted': 0
-        }
-      )
+
+      //Retreive next active group id for a user
+      String groupID = await RimeFunctions.getAvailableChannelGroup(userID);
+
+      //Create a channel memebership
+      RimeChannelMemebership membership = RimeChannelMemebership(
+        notifications: true,
+        readAction: true,
+        accepted: Rime.functions.chatAccepted(RimeRepository().userID, users),
+        deleted: 0
       );
+
+      //Create memebership data
+      members.add(
+        ChannelMemberMetadataInput(
+          userID,
+          custom: membership.toJson()
+        )
+      );
+
+      //Add the channel to the user specific group
+      await client.channelGroups.addChannels(groupID, Set.from([channelID]));
+
+      // retreive data for login user
+      if(userID == RimeRepository().userID){
+        userGroupID = groupID;
+        userMembership = membership;
+      }
     }
     await client.objects.setChannelMembers(channelID, members);
 
-    String userGroupID;
-
-    //Assigns channel into available groups
-    for (String userID in users) {
-      String groupID = await RimeFunctions.getAvailableChannelGroup(userID);
-      if(userID == RimeRepository().userID){
-        userGroupID = groupID;
-      }
-      await client.channelGroups.addChannels(groupID, Set.from([channelID]));
+    //No channel made for the loggedIn user
+    if(userGroupID == null || userMembership == null){
+      return null;
     }
 
-    return userGroupID;
+    //Retrun channel for logged in user
+    RimeChannel createdChannel = RimeChannel(
+      channel: channelID,
+      lastUpdated: time.value,
+      membership: userMembership,
+      groupId: userGroupID
+    );
+
+    return createdChannel;
   }
 
   static RimeChannel getChannel(String channel) {
